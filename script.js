ea5ee6a98441472e2ecba0f8e333401c734235d0
giovaniCheloni <130862039+giovaniCheloni@users.noreply.github.com>
2024-10-21 19:02:52 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -9,9 +9,14 @@ const cartCounter = document.getElementById("cart-count")
 const addressInput = document.getElementById("address")
 const addressWarn = document.getElementById("address-warn")
 
+
+let cart = []
+
 // Abrir o modal do carrinho
 cartBtn.addEventListener("click", function(){
+    updateCartModal();
     cartModal.style.display = "flex"
+    
 })
 
 // Fechar o modal quando clicar fora
@@ -25,3 +30,179 @@ cartModal.addEventListener("click", function(event){
 closeModalBtn.addEventListener("click", function(){
     cartModal.style.display = "none"
 })
+
+menu.addEventListener("click", function(event){
+    //console.log(event.target)
+
+    let parentButton = event.target.closest(".add-to-cart-btn")
+
+    console.log(parentButton);
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+        
+        //Adicionar no carrinho.
+        addToCart(name, price)
+        
+    }
+
+})
+
+
+// função para adição no carrinho.
+function addToCart(name, price){
+    const existingItem = cart.find(item => item.name === name)
+
+    if(existingItem){
+        //Se o item já existe, aumenta apenas a quantidade + 1.
+        existingItem.quantity += 1;
+        
+    }else{
+
+        cart.push({
+        name,
+        price,
+        quantity: 1,
+    })
+}
+
+updateCartModal()
+
+}
+
+//Atualiza o carrinho (modal).
+function updateCartModal(){
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
+
+        cartItemElement.innerHTML = `
+        <div class="flex items-center justify-between">
+            <div>
+                <p class="font-bold mt-2">${item.name}</p>
+                <p>Qtd: ${item.quantity}</p>
+                <p class="font-medium">R$ ${item.price.toFixed(2)}</p>
+            </div>
+           
+                <button class="remove-from-cart-btn" data-name="${item.name}">
+                    Remover                
+                </button>
+                
+        </div>
+        `
+
+        total += item.price * item.quantity;
+
+        cartItemsContainer.appendChild(cartItemElement)
+
+    })
+
+    cartTotal.textContent = total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
+
+    cartCounter.innerHTML = cart.length;
+}
+
+//Função para remover items do carrinho(modal).
+cartItemsContainer.addEventListener("click", function (event){
+    if(event.target.classList.contains("remove-from-cart-btn")){
+        const name = event.target.getAttribute("data-name")
+
+     removeItemCart(name);
+    }    
+})
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1){
+        const item = cart[index];
+        
+        if(item.quantity > 1){
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+
+        cart.splice(index, 1);
+        updateCartModal();
+    }
+}
+
+//Função para pegar oque está escrito no input do carrinho (Modal).
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+})
+
+//Finalizar pedido.
+checkoutBtn.addEventListener("click", function(){
+    const isOpen = checkRestauranteOpen();
+    if(!isOpen){
+
+    Toastify({
+        text: "Ops! O Restaurante está fechado!",
+        duration: 3000,       
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "#ef4444",
+        },       
+      }).showToast();
+
+        return;
+    }
+
+
+    if(cart.length === 0) return;
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return;
+    }
+
+    //Enviar o pedido para API Whats
+    const cartItems = cart.map((item) => {
+        return (
+            ` ${item.name} Quantidade: ${item.quantity} preço: R$${item.price} |`
+        )
+    }).join("")
+
+    const message = encodeURIComponent(cartItems)
+    const phone = "31992305938"
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blanck")
+
+    cart = [];
+    updateCartModal();
+})
+
+// Verificar a hora e manipular o card horário.
+function checkRestauranteOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22;
+    //true = Restaurante Aberto
+}
+
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestauranteOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-600");
+    spanItem.classList.add("bg-green-600")
+}else{
+    spanItem.classList.remove("bg-green-600")
+    spanItem.classList.add("bg-red-600")
+}
